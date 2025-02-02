cd499a8a12c17e6c5792371ea6f06c315a61c2cb
Ramon <ramonburgstad2@gmail.com>
2025-02-02 18:12:38 +0100

    Home pagina

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "bootstrap/dist/css/bootstrap.css";
-// import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <App /> */}
     <div>
-      <h1 style={{ backgroundColor: "#BB0000", color: "#99AABB" }}>Testing</h1>
+      {/* <h1 style={{ backgroundColor: "#BB0000", color: "#99AABB" }}>Testing</h1> */}
       <div
         style={{ alignContent: "center", textAlign: "center", height: "100vh" }}
       >
         <p>Hallo</p>
       </div>
     </div>
+    <App />
   </StrictMode>
 );
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -3,14 +3,14 @@ const Home = () => {
     <>
       {/* <h1>Testing</h1> */}
       {/* <nav classNameName="navbar"></nav> */}
-      {/* <nav
+      <nav
         className="navbar fixed-top bg-dark border-bottom border-body"
         data-bs-theme="dark"
       >
         <div className="container-fluid">
           <span className="navbar-brand mb-0 h1">Navbar</span>
         </div>
-      </nav> */}
+      </nav>
       <h1 className="button-container">Hallo</h1>
       {/* <div classNameName="button-container">Hallo</div> */}
     </>
