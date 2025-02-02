7a3a9cf6eb4fe66f1b7218c990a9f486e26f9d40
Ramon <ramonburgstad2@gmail.com>
2025-02-02 20:28:48 +0100

    added pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,15 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App.tsx";
 
+// Color scheme:
+// licht groen: BDDAD5
+// Licht blauw: 84ADEA
+// Grijs achtig: 8099C8
+// Zwart: 212514
+// Wit: F5EDE2
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div>
-      {/* <h1 style={{ backgroundColor: "#BB0000", color: "#99AABB" }}>Testing</h1> */}
-      <div
-        style={{ alignContent: "center", textAlign: "center", height: "100vh" }}
-      >
-        <p>Hallo</p>
-      </div>
-    </div>
     <App />
   </StrictMode>
 );
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,20 +1,56 @@
-const Home = () => {
+import { useState } from "react";
+
+function Home() {
+  const [selectedPage, setSelectedPage] = useState("home");
+
+  const renderContent = () => {
+    if (selectedPage === "home") {
+      return (
+        <p style={{ backgroundColor: "blue", height: "100vh" }}>
+          Welcome to the Home page!
+        </p>
+      );
+    } else if (selectedPage === "planning") {
+      return <p>Here is the Planning page.</p>;
+    } else {
+      return <p>Select a page from the appbar.</p>;
+    }
+  };
+
   return (
     <>
-      {/* <h1>Testing</h1> */}
-      {/* <nav classNameName="navbar"></nav> */}
-      <nav
-        className="navbar fixed-top bg-dark border-bottom border-body"
-        data-bs-theme="dark"
-      >
-        <div className="container-fluid">
-          <span className="navbar-brand mb-0 h1">Navbar</span>
+      <div style={{ height: "100vh" }}>
+        <div
+          style={{
+            height: "50px",
+            textAlign: "center",
+            lineHeight: "50px",
+            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+            backgroundColor: "#333",
+            color: "white",
+          }}
+        >
+          <div className="row">
+            <p
+              className="col"
+              style={{ cursor: "pointer" }}
+              onClick={() => setSelectedPage("home")}
+            >
+              Home
+            </p>
+            <p
+              className="col"
+              style={{ cursor: "pointer" }}
+              onClick={() => setSelectedPage("planning")}
+            >
+              Planning
+            </p>
+          </div>
         </div>
-      </nav>
-      <h1 className="button-container">Hallo</h1>
-      {/* <div classNameName="button-container">Hallo</div> */}
+        <div style={{ padding: "20px" }}>{renderContent()} </div>
+      </div>
     </>
   );
-};
+}
 
 export default Home;
